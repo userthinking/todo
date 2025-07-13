0f27942c9119e74c9bd06c85c894d24c76c615be
userthinking <siddharthabhumigari@gmail.com>
2025-07-14 02:47:08 +0530

    updated the UI

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -18,7 +18,7 @@ const TodoItem = ({ todo }) => {
 
   return (
     <div
-      className={`w-xl flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+      className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
         todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
       }`}
     >
